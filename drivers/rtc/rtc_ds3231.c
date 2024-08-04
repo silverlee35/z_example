edc507a6fb3f8505a86fe7229d3f332a9f74130f
Gergo Vari <work@varigergo.hu>
2024-08-04 18:18:31 +0200

    drivers: rtc: maxim,ds3231: refactor i2c
    This refactor allows i2c code to be reused, thus
    giving easy access to register reading/writing.
    
    Signed-off-by: Gergo Vari <work@varigergo.hu>

--- drivers/rtc/rtc_ds3231.c
+++ drivers/rtc/rtc_ds3231.c
@@ -5,34 +5,129 @@
  * Copyright (c) 2024 Gergo Vari <work@varigergo.hu>
  */
 
-#include <zephyr/drivers/i2c.h>
+/* TODO: reimplement set_time, get_time */
+/* TODO: implement control */
+/* TODO: implement control status */
+/* TODO: implement get_temp */
+/* TODO: implement 24h/ampm modes */
+/* TODO: handle century bit */
+/* TODO: decide if we need to deal with aging offset */
+/* TODO: decide if we need to deal with CONV */
+/* TODO: implement device power management */
+
 #include <zephyr/drivers/rtc.h>
-#include <zephyr/logging/log.h>
 #include <zephyr/pm/device.h>
 #include <zephyr/sys/util.h>
 
 #define DT_DRV_COMPAT maxim_ds3231
 
+#include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(ds3231, CONFIG_RTC_LOG_LEVEL);
 
-/* DS3231 registers */
-#define DS3231_REG_SECONDS 0x00
-#define DS3231_REG_MINUTES 0x01
-#define DS3231_REG_HOURS   0x02
-#define DS3231_REG_DAY     0x03
-#define DS3231_REG_DATE    0x04
-#define DS3231_REG_MONTH   0x05
-#define DS3231_REG_YEAR    0x06
-#define DS3231_REG_CTRL    0x07
+#include <zephyr/drivers/i2c.h>
+/*           *
+ * REGISTERS *
+ *           */
 
-#define SECONDS_BITS  GENMASK(6, 0)
-#define MINUTES_BITS  GENMASK(7, 0)
-#define HOURS_BITS    GENMASK(5, 0)
-#define DATE_BITS     GENMASK(5, 0)
-#define MONTHS_BITS   GENMASK(4, 0)
-#define WEEKDAY_BITS  GENMASK(2, 0)
-#define YEAR_BITS     GENMASK(7, 0)
-#define VALIDATE_24HR BIT(6)
+/* Time registers */
+#define DS3231_REG_TIME_SECONDS     0x00
+#define DS3231_REG_TIME_MINUTES     0x01
+#define DS3231_REG_TIME_HOURS       0x02
+#define DS3231_REG_TIME_DAY_OF_WEEK 0x03
+#define DS3231_REG_TIME_DATE        0x04
+#define DS3231_REG_TIME_MONTH       0x05
+#define DS3231_REG_TIME_YEAR        0x06
+
+/* Alarm 1 registers */
+#define DS3231_REG_ALARM_1_SECONDS 0x07
+#define DS3231_REG_ALARM_1_MINUTES 0x08
+#define DS3231_REG_ALARM_1_HOURS   0x09
+#define DS3231_REG_ALARM_1_DATE    0x0A 
+
+/* Alarm 2 registers */
+/* Alarm 2 has no seconds to set, it only has minute accuracy. */
+#define DS3231_REG_ALARM_2_MINUTES 0x0B
+#define DS3231_REG_ALARM_2_HOURS   0x0C
+#define DS3231_REG_ALARM_2_DATE    0x0D
+
+/* Control registers */
+#define DS3231_REG_CTRL         0x0E
+#define DS3231_REG_CTRL_STATUS  0x0F
+
+/* Aging offset register */
+#define DS3231_REG_AGING_OFFSET 0x10
+
+/* Temperature registers */
+#define DS3231_REG_TEMP_MSB 0x11
+#define DS3231_REG_TEMP_LSB 0x12
+
+
+
+/*           *
+ * BITMASKS  *
+ *           */
+
+/* Time bitmasks */
+#define DS3231_BITS_TIME_SECONDS     GENMASK(6, 0) 
+#define DS3231_BITS_TIME_MINUTES     GENMASK(6, 0)
+#define DS3231_BITS_TIME_HOURS       GENMASK(5, 0) 
+#define DS3231_BITS_TIME_AMPM        BIT(5)
+#define DS3231_BITS_TIME_12HR        BIT(6)
+#define DS3231_BITS_TIME_DAY_OF_WEEK GENMASK(2, 0)
+#define DS3231_BITS_TIME_DATE        GENMASK(5, 0) 
+#define DS3231_BITS_TIME_MONTH       GENMASK(4, 0) 
+#define DS3231_BITS_TIME_CENTURY     BIT(7)
+#define DS3231_BITS_TIME_YEAR        GENMASK(7, 0)
+
+/* Alarm bitmasks */
+/* All alarm bitmasks match with time other than date and the alarm rate bit. */
+#define DS3231_BITS_ALARM_RATE        BIT(7)
+#define DS3231_BITS_ALARM_DATE_W_OR_M BIT(6)
+
+/* Control bitmasks */
+#define DS3231_BITS_SIGN BIT(7)
+#define DS3231_BITS_EOSC BIT(7) /* enable oscillator, active low */
+#define DS3231_BITS_SQW  BIT(6) /* enable square-wave */
+
+/*  Setting the CONV bit to 1 forces the temperature sensor to 
+ *  convert the temperature into digital code and 
+ *  execute the TCXO algorithm to update 
+ *  the capacitance array to the oscillator. This can only 
+ *  happen when a conversion is not already in progress. 
+ *  The user should check the status bit BSY before 
+ *  forcing the controller to start a new TCXO execution. 
+ *  A user-initiated temperature conversion 
+ *  does not affect the internal 64-second update cycle. */
+#define DS3231_BITS_CONV BIT(6) 
+
+/* Rate selectors */
+/* TODO: the datasheet is probably wrong and it's not 1Hz but 1kHz, should check though */
+/* RS2 | RS1 | SQW FREQ
+ *  0  |  0  | 1Hz
+ *  0  |  1  | 1.024kHz
+ *  1  |  0  | 4.096kHz
+ *  1  |  1  | 8.192kHz */
+#define DS3231_BITS_RS2 BIT(4) 
+#define DS3231_BITS_RS1 BIT(3) 
+
+#define DS3231_BITS_INTCTRL    BIT(2) 
+#define DS3231_BITS_ALARM_2_EN BIT(1) 
+#define DS3231_BITS_ALARM_1_EN BIT(0) 
+
+/* Control status bitmasks */
+/* For some reason you can access OSF in both control and control status registers. */
+#define DS3231_BITS_OSF          BIT(7) /* oscillator stop flag */
+#define DS3231_BITS_32_EN        BIT(3) /* 32kHz square-wave */
+#define DS3231_BITS_BSY          BIT(2) /* set when TXCO is busy, see CONV flag */
+#define DS3231_BITS_ALARM_2_FLAG BIT(1)
+#define DS3231_BITS_ALARM_1_FLAG BIT(0)
+
+/* Aging offset bitmask */
+#define DS3231_BITS_DATA BIT(6, 0)
+
+/* Temperature bitmasks */
+#define DS3231_BITS_TEMP_MSB GENMASK(6, 0) /* integer portion */
+#define DS3231_BITS_TEMP_LSB GENMASK(7, 6) /* fractional portion */
 
 struct ds3231_config {
 	struct i2c_dt_spec i2c_bus;
@@ -42,85 +137,106 @@ struct ds3231_data {
 	struct k_spinlock lock;
 };
 
-static int ds3231_set_time(const struct device *dev, const struct rtc_time *tm)
-{
+static int i2c_set_registers(const struct device *dev, uint8_t start_reg, const uint8_t *buf, const size_t buf_size) {
 	int err;
-	uint8_t regs[7];
-
 	struct ds3231_data *data = dev->data;
 	const struct ds3231_config *config = dev->config;
 
 	k_spinlock_key_t key = k_spin_lock(&data->lock);
 
+	err = i2c_burst_write_dt(&config->i2c_bus, start_reg, buf, buf_size);
+
+	k_spin_unlock(&data->lock, key);
+	return err;
+}
+
+static int i2c_get_registers(const struct device *dev, uint8_t start_reg, uint8_t *buf, const size_t buf_size) {
+	int err;
+	struct ds3231_data *data = dev->data;
+	const struct ds3231_config *config = dev->config;
+
+	k_spinlock_key_t key = k_spin_lock(&data->lock);
+
+	err = i2c_burst_read_dt(&config->i2c_bus, start_reg, buf, buf_size);
+
+	k_spin_unlock(&data->lock, key);
+	return err;
+}
+
+static int ds3231_set_time(const struct device *dev, const struct rtc_time *tm)
+{
 	LOG_DBG("set time: year = %d, mon = %d, mday = %d, wday = %d, hour = %d, "
 		"min = %d, sec = %d",
 		tm->tm_year, tm->tm_mon, tm->tm_mday, tm->tm_wday, tm->tm_hour, tm->tm_min,
 		tm->tm_sec);
 
-	regs[0] = bin2bcd(tm->tm_sec) & SECONDS_BITS;
-	regs[1] = bin2bcd(tm->tm_min);
-	regs[2] = bin2bcd(tm->tm_hour);
-	regs[3] = bin2bcd(tm->tm_wday);
-	regs[4] = bin2bcd(tm->tm_mday);
-	regs[5] = bin2bcd(tm->tm_mon);
-	regs[6] = bin2bcd((tm->tm_year % 100));
+	const size_t buf_size = 7;
+	uint8_t buf[buf_size];
+	buf[0] = bin2bcd(tm->tm_sec)  & DS3231_BITS_TIME_SECONDS;
+	buf[1] = bin2bcd(tm->tm_min)  & DS3231_BITS_TIME_MINUTES;
+	buf[2] = bin2bcd(tm->tm_hour) & DS3231_BITS_TIME_HOURS;
+	buf[3] = bin2bcd(tm->tm_wday) & DS3231_BITS_TIME_DAY_OF_WEEK;
+	buf[4] = bin2bcd(tm->tm_mday) & DS3231_BITS_TIME_DATE;
+	buf[5] = bin2bcd(tm->tm_mon)  & DS3231_BITS_TIME_MONTH;
+	
+	/* here modulo 100 returns the last two digits of the year,
+	 * as the DS3231 chip can only store year data for 0-99,
+	 * hitting that ceiling can be detected with the century bit. */
+	buf[6] = bin2bcd((tm->tm_year % 100)) & DS3231_BITS_TIME_YEAR; 
 
-	err = i2c_burst_write_dt(&config->i2c_bus, DS3231_REG_SECONDS, regs, sizeof(regs));
-
-	k_spin_unlock(&data->lock, key);
-
-	return err;
+	return i2c_set_registers(dev, DS3231_REG_TIME_SECONDS, buf, buf_size);
 }
 
 static int ds3231_get_time(const struct device *dev, struct rtc_time *timeptr)
 {
-	int err;
-	uint8_t regs[7];
-
-	struct ds3231_data *data = dev->data;
-	const struct ds3231_config *config = dev->config;
-
-	k_spinlock_key_t key = k_spin_lock(&data->lock);
-
-	err = i2c_burst_read_dt(&config->i2c_bus, DS3231_REG_SECONDS, regs, sizeof(regs));
+	const size_t buf_size = 7;
+	uint8_t buf[buf_size];
+	int err = i2c_get_registers(dev, DS3231_REG_TIME_SECONDS, buf, buf_size);
 	if (err != 0) {
-		goto unlock;
+		return err;
 	}
 
-	timeptr->tm_sec = bcd2bin(regs[0] & SECONDS_BITS);
-	timeptr->tm_min = bcd2bin(regs[1] & MINUTES_BITS);
-	timeptr->tm_hour = bcd2bin(regs[2] & HOURS_BITS); /* 24hr mode */
-	timeptr->tm_wday = bcd2bin(regs[3] & WEEKDAY_BITS);
-	timeptr->tm_mday = bcd2bin(regs[4] & DATE_BITS);
-	timeptr->tm_mon = bcd2bin(regs[5] & MONTHS_BITS);
-	timeptr->tm_year = bcd2bin(regs[6] & YEAR_BITS);
+	timeptr->tm_sec = bcd2bin(buf[0] & DS3231_BITS_TIME_SECONDS);
+	timeptr->tm_min = bcd2bin(buf[1] & DS3231_BITS_TIME_MINUTES);
+	timeptr->tm_hour = bcd2bin(buf[2] & DS3231_BITS_TIME_HOURS);
+	timeptr->tm_wday = bcd2bin(buf[3] & DS3231_BITS_TIME_DAY_OF_WEEK);
+	timeptr->tm_mday = bcd2bin(buf[4] & DS3231_BITS_TIME_DATE);
+	timeptr->tm_mon = bcd2bin(buf[5] & DS3231_BITS_TIME_MONTH);
+	timeptr->tm_year = bcd2bin(buf[6] & DS3231_BITS_TIME_YEAR);
 	timeptr->tm_year = timeptr->tm_year + 100;
-
-	/* Not used */
+	
 	timeptr->tm_nsec = 0;
 	timeptr->tm_isdst = -1;
 	timeptr->tm_yday = -1;
-
-	/* Validate the chip in 24hr mode */
-	if (regs[2] & VALIDATE_24HR) {
-		err = -ENODATA;
-		goto unlock;
-	}
 
 	LOG_DBG("get time: year = %d, mon = %d, mday = %d, wday = %d, hour = %d, "
 		"min = %d, sec = %d",
 		timeptr->tm_year, timeptr->tm_mon, timeptr->tm_mday, timeptr->tm_wday,
 		timeptr->tm_hour, timeptr->tm_min, timeptr->tm_sec);
 
-unlock:
-	k_spin_unlock(&data->lock, key);
-
-	return err;
+	return 0;
 }
 
 static const struct rtc_driver_api ds3231_driver_api = {
 	.set_time = ds3231_set_time,
 	.get_time = ds3231_get_time,
+
+#ifdef CONFIG_RTC_ALARM
+	/*.alarm_get_supported_fields = ds3231_alarm_get_supported_fields,
+	.alarm_set_time = ds3231_alarm_set_time,
+	.alarm_get_time = ds3231_alarm_get_time,
+	.alarm_is_pending = ds3231_alarm_is_pending,
+	.alarm_set_callback = ds3231_alarm_set_callback,*/
+#endif /* CONFIG_RTC_ALARM */
+
+#ifdef CONFIG_RTC_UPDATE
+	/*.update_set_callback = ds3231_update_set_callback,*/
+#endif /* CONFIG_RTC_UPDATE */
+
+#ifdef CONFIG_RTC_CALIBRATION
+	/*.set_calibration = ds3231_set_calibration,
+	.get_calibration = ds3231_get_calibration,*/
+#endif /* CONFIG_RTC_CALIBRATION */
 };
 
 static int ds3231_init(const struct device *dev)
@@ -129,18 +245,18 @@ static int ds3231_init(const struct device *dev)
 	const struct ds3231_config *config = dev->config;
 
 	if (!i2c_is_ready_dt(&config->i2c_bus)) {
-		LOG_ERR("I2C bus not ready");
+		LOG_ERR("I2C bus not ready.");
 		return -ENODEV;
 	}
 
 	/* Disable squarewave output */
 	err = i2c_reg_write_byte_dt(&config->i2c_bus, DS3231_REG_CTRL, 0x00);
 	if (err < 0) {
-		LOG_ERR("Error: SQW:%d\n", err);
+		LOG_ERR("Error: disabling SQW: %d\n", err);
 	}
 
 	/* Make clock halt = 0 */
-	err = i2c_reg_write_byte_dt(&config->i2c_bus, DS3231_REG_SECONDS, 0x00);
+	err = i2c_reg_write_byte_dt(&config->i2c_bus, DS3231_REG_TIME_SECONDS, 0x00);
 	if (err < 0) {
 		LOG_ERR("Error: Set clock halt bit:%d\n", err);
 	}
