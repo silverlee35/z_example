dztang/zephyr

tests: bsim: Bluetooth: broadcast_audio_source interleaved/sequential
Test broadcast audio source sample in interleaved and
sequential packing.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

tests/bsim/bluetooth/audio_samples/broadcast_audio_sink/tests_scripts/broadcast_audio_sequential.sh
@@ -1,0 +1,29 @@
+#!/usr/bin/env bash
+# Copyright 2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+# Simple selfchecking test for the broadcast audio sink/source samples,
+# It relies on the bs_tests hooks to register a test timer callback, which after a deadline
+# will check how many audio packets the broadcast audio sink has received, and if over a threshold
+# it considers the test passed
+
+simulation_id="broadcast_audio_samples_sequential_test"
+verbosity_level=2
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+EXECUTE_TIMEOUT=200
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD}_samples_bluetooth_broadcast_audio_source_prj_conf_overlay-sequential_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_samples_broadcast_audio_sink_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 \
+  -testid=broadcast_audio_sink
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=120e6 $@
+
+wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails

tests/bsim/bluetooth/audio_samples/compile.sh
@@ -21,7 +21,10 @@ source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
 if [ "${BOARD}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
   app=samples/bluetooth/unicast_audio_server sysbuild=1 compile
-  app=samples/bluetooth/broadcast_audio_source sysbuild=1 compile
+  app=samples/bluetooth/broadcast_audio_source conf_overlay=overlay-sequential.conf \
+    sysbuild=1 compile
+  app=samples/bluetooth/broadcast_audio_source conf_overlay=overlay-interleaved.conf \
+    sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/unicast_audio_client sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/broadcast_audio_sink sysbuild=1 \
     conf_file=${ZEPHYR_BASE}/samples/bluetooth/broadcast_audio_sink/prj.conf \
@@ -29,8 +32,12 @@ if [ "${BOARD}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
 else
   app=samples/bluetooth/unicast_audio_server conf_overlay=overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD}_${app}_prj_conf sysbuild=1 compile
-  app=samples/bluetooth/broadcast_audio_source conf_overlay=overlay-bt_ll_sw_split.conf \
-    exe_name=bs_${BOARD}_${app}_prj_conf sysbuild=1 compile
+  app=samples/bluetooth/broadcast_audio_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-sequential.conf' \
+    exe_name=bs_${BOARD}_${app}_prj_conf_overlay-sequential_conf sysbuild=1 compile
+  app=samples/bluetooth/broadcast_audio_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-interleaved.conf' \
+    exe_name=bs_${BOARD}_${app}_prj_conf_overlay-interleaved_conf sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/unicast_audio_client \
     conf_overlay=${ZEPHYR_BASE}/samples/bluetooth/unicast_audio_client/overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD}_${app}_prj_conf sysbuild=1 compile

silverlee35/z_example

tests/bsim/bluetooth/audio_samples/bap_broadcast_sink/tests_scripts/broadcast_audio_sequential.sh
@@ -1,0 +1,28 @@
+#!/usr/bin/env bash
+# Copyright 2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: Apache-2.0
+
+# Simple selfchecking test for the broadcast audio sink/source samples,
+# It relies on the bs_tests hooks to register a test timer callback, which after a deadline
+# will check how many audio packets the broadcast audio sink has received, and if over a threshold
+# it considers the test passed
+
+simulation_id="samples_bluetooth_bap_broadcast_source_sequential_test"
+verbosity_level=2
+EXECUTE_TIMEOUT=200
+
+source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
+
+cd ${BSIM_OUT_PATH}/bin
+
+Execute ./bs_${BOARD_TS}_samples_bluetooth_bap_broadcast_source_prj_conf_overlay-sequential_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=0 -RealEncryption=1
+
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_audio_samples_bap_broadcast_sink_prj_conf \
+  -v=${verbosity_level} -s=${simulation_id} -d=1 -RealEncryption=1 \
+  -testid=bap_broadcast_sink
+
+Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \
+  -D=2 -sim_length=120e6 $@
+
+wait_for_background_jobs #Wait for all programs in background and return != 0 if any fails

tests/bsim/bluetooth/audio_samples/compile.sh
@@ -13,7 +13,10 @@ source ${ZEPHYR_BASE}/tests/bsim/compile.source
 
 if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
   app=samples/bluetooth/bap_unicast_server sysbuild=1 compile
-  app=samples/bluetooth/bap_broadcast_source sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-sequential.conf \
+    sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-interleaved.conf \
+    sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/bap_unicast_client \
     sample=${ZEPHYR_BASE}/samples/bluetooth/bap_unicast_client \
     conf_file=${sample}/prj.conf \
@@ -41,8 +44,12 @@ if [ "${BOARD_TS}" == "nrf5340bsim_nrf5340_cpuapp" ]; then
 else
   app=samples/bluetooth/bap_unicast_server conf_overlay=overlay-bt_ll_sw_split.conf \
     exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
-  app=samples/bluetooth/bap_broadcast_source conf_overlay=overlay-bt_ll_sw_split.conf \
-    exe_name=bs_${BOARD_TS}_${app}_prj_conf sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-sequential.conf' \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf_overlay-sequential_conf sysbuild=1 compile
+  app=samples/bluetooth/bap_broadcast_source \
+    conf_overlay='overlay-bt_ll_sw_split.conf;overlay-interleaved.conf' \
+    exe_name=bs_${BOARD_TS}_${app}_prj_conf_overlay-interleaved_conf sysbuild=1 compile
   app=tests/bsim/bluetooth/audio_samples/bap_unicast_client \
     sample=${ZEPHYR_BASE}/samples/bluetooth/bap_unicast_client \
     conf_file=${sample}/prj.conf \