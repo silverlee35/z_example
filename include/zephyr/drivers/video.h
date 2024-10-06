18adf055b6d3d33252bfe435249501d9ecdd030c
Josuah Demangeon <me@josuah.net>
2024-10-06 18:19:44 +0000

    drivers: video: Make VIDEO_FOURCC() a documented API
    Rename video_fourcc() to VIDEO_FOURCC(), differing from the Linux
    implementation, but more compliant with the coding style.
    
    Also introduce a VIDEO_FOURCC_FROM_STR() to generate a FOURCC format
    code out of a 4-characters string.
    
    Signed-off-by: Josuah Demangeon <me@josuah.net>

--- include/zephyr/drivers/video.h
+++ include/zephyr/drivers/video.h
@@ -726,9 +726,22 @@ struct video_buffer *video_buffer_alloc(size_t size);
  */
 void video_buffer_release(struct video_buffer *buf);
 
-/* fourcc - four-character-code */
-#define video_fourcc(a, b, c, d)                                                                   \
+/**
+ * @brief Four-character-code uniquely identifying the pixel format
+ */
+#define VIDEO_FOURCC(a, b, c, d)                                                                   \
 	((uint32_t)(a) | ((uint32_t)(b) << 8) | ((uint32_t)(c) << 16) | ((uint32_t)(d) << 24))
+
+/**
+ * @brief Convert a four-character string to a four-character-code
+ *
+ * This converts a string literal or variable into a four-character-code
+ * as defined by @ref VIDEO_FOURCC.
+ *
+ * @param str String to be converted
+ * @return Four-character-code.
+ */
+#define VIDEO_FOURCC_FROM_STR(str) VIDEO_FOURCC((str)[0], (str)[1], (str)[2], (str)[3])
 
 /**
  * @defgroup video_pixel_formats Video pixel formats
