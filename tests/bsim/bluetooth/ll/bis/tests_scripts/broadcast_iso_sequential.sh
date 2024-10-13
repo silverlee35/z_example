dztang/zephyr

tests: bsim: Bluetooth: Test for single BIS with interleaved packing
Test for single BIS with interleaved packing or sequential
packing requested.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

tests/bsim/bluetooth/ll/bis/tests_scripts/broadcast_iso_sequential.sh
@@ -6,16 +6,16 @@ source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
 # Basic ISO broadcast test: a broadcaster transmits a BIS and a receiver listens
 # to the BIS.
-simulation_id="broadcast_iso"
+simulation_id="broadcast_iso_sequential"
 verbosity_level=2
 EXECUTE_TIMEOUT=30
 
 cd ${BSIM_OUT_PATH}/bin
 
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_bis_prj_conf \
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_bis_prj_conf_overlay-sequential_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=0 -testid=receive
 
-Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_bis_prj_conf \
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_ll_bis_prj_conf_overlay-sequential_conf \
   -v=${verbosity_level} -s=${simulation_id} -d=1 -testid=broadcast
 
 Execute ./bs_2G4_phy_v1 -v=${verbosity_level} -s=${simulation_id} \

silverlee35/z_example

tests/bsim/bluetooth/ll/bis/tests_scripts/broadcast_iso_sequential.sh
@@ -5,14 +5,14 @@
 source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
 # Basic ISO broadcast test: a broadcaster transmits a BIS and a receiver listens
-# to the BIS, and recevied SDUs are emitted via vendor data path implementation.
-simulation_id="broadcast_iso_vs_dp"
+# to the BIS.
+simulation_id="broadcast_iso_sequential"
 verbosity_level=2
 
 cd ${BSIM_OUT_PATH}/bin
 
-Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_bis_prj_vs_dp_conf \
-  -v=${verbosity_level} -s=${simulation_id} -RealEncryption=1 -d=0 -testid=receive_vs_dp
+Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_bis_prj_conf_overlay-sequential_conf \
+  -v=${verbosity_level} -s=${simulation_id} -RealEncryption=1 -d=0 -testid=receive
 
 Execute ./bs_${BOARD_TS}_tests_bsim_bluetooth_ll_bis_prj_conf_overlay-sequential_conf \
   -v=${verbosity_level} -s=${simulation_id} -RealEncryption=1 -d=1 -testid=broadcast