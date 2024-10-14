Johnsonyu0926/zephyr

drivers: video: controls: rename controls to Linux-like names
This uses Linux V4L2 controls as a reference to give names to the
CIDs. Apply the renaming down to the drivers that use them.

Signed-off-by: Josuah Demangeon <me@josuah.net>

include/zephyr/drivers/video-controls.h
@@ -61,14 +61,14 @@ extern "C" {
  * @name Base class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CTRL_CLASS_BASE + 0)
-#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CTRL_CLASS_BASE + 1)
-#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CTRL_CLASS_BASE + 2)
-#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CTRL_CLASS_BASE + 17)
-#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CTRL_CLASS_BASE + 19)
-#define VIDEO_CID_HFLIP			(VIDEO_CTRL_CLASS_BASE + 20)
-#define VIDEO_CID_VFLIP			(VIDEO_CTRL_CLASS_BASE + 21)
-#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CTRL_CLASS_BASE + 26)
+#define VIDEO_CID_BRIGHTNESS			(VIDEO_CTRL_CLASS_BASE + 0)
+#define VIDEO_CID_CONTRAST			(VIDEO_CTRL_CLASS_BASE + 1)
+#define VIDEO_CID_SATURATION			(VIDEO_CTRL_CLASS_BASE + 2)
+#define VIDEO_CID_EXPOSURE			(VIDEO_CTRL_CLASS_BASE + 17)
+#define VIDEO_CID_GAIN				(VIDEO_CTRL_CLASS_BASE + 19)
+#define VIDEO_CID_HFLIP				(VIDEO_CTRL_CLASS_BASE + 20)
+#define VIDEO_CID_VFLIP				(VIDEO_CTRL_CLASS_BASE + 21)
+#define VIDEO_CID_WHITE_BALANCE_TEMPERATURE	(VIDEO_CTRL_CLASS_BASE + 26)
 /**
  * @}
  */
@@ -77,7 +77,7 @@ extern "C" {
  * @name Camera class controls IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CTRL_CLASS_CAMERA_BASE + 13)
+#define VIDEO_CID_ZOOM_ABSOLUTE			(VIDEO_CTRL_CLASS_CAMERA_BASE + 13)
 /**
  * @}
  */
@@ -86,7 +86,7 @@ extern "C" {
  * @name JPEG class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CTRL_CLASS_JPEG_BASE + 3)
+#define VIDEO_CID_JPEG_COMPRESSION_QUALITY	(VIDEO_CTRL_CLASS_JPEG_BASE + 3)
 /**
  * @}
  */
@@ -95,7 +95,7 @@ extern "C" {
  * @name Image Processing class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CTRL_CLASS_IMAGE_PROC_BASE + 3)
+#define VIDEO_CID_TEST_PATTERN			(VIDEO_CTRL_CLASS_IMAGE_PROC_BASE + 3)
 /**
  * @}
  */

silverlee35/z_example

include/zephyr/drivers/video-controls.h
@@ -61,23 +61,23 @@ extern "C" {
  * @name Base class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CID_BASE + 0)
-#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CID_BASE + 1)
-#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CID_BASE + 2)
-#define VIDEO_CID_CAMERA_HUE		(VIDEO_CID_BASE + 3)
-#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CID_BASE + 17)
-#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CID_BASE + 19)
-#define VIDEO_CID_HFLIP			(VIDEO_CID_BASE + 20)
-#define VIDEO_CID_VFLIP			(VIDEO_CID_BASE + 21)
+#define VIDEO_CID_BRIGHTNESS			(VIDEO_CID_BASE + 0)
+#define VIDEO_CID_CONTRAST			(VIDEO_CID_BASE + 1)
+#define VIDEO_CID_SATURATION			(VIDEO_CID_BASE + 2)
+#define VIDEO_CID_HUE				(VIDEO_CID_BASE + 3)
+#define VIDEO_CID_EXPOSURE			(VIDEO_CID_BASE + 17)
+#define VIDEO_CID_GAIN				(VIDEO_CID_BASE + 19)
+#define VIDEO_CID_HFLIP				(VIDEO_CID_BASE + 20)
+#define VIDEO_CID_VFLIP				(VIDEO_CID_BASE + 21)
 /** Power line frequency (enum) filter to avoid flicker */
-#define VIDEO_CID_POWER_LINE_FREQUENCY	(VIDEO_CID_BASE + 24)
+#define VIDEO_CID_POWER_LINE_FREQUENCY		(VIDEO_CID_BASE + 24)
 enum video_power_line_frequency {
 	VIDEO_CID_POWER_LINE_FREQUENCY_DISABLED = 0,
 	VIDEO_CID_POWER_LINE_FREQUENCY_50HZ = 1,
 	VIDEO_CID_POWER_LINE_FREQUENCY_60HZ = 2,
 	VIDEO_CID_POWER_LINE_FREQUENCY_AUTO = 3,
 };
-#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CID_BASE + 26)
+#define VIDEO_CID_WHITE_BALANCE_TEMPERATURE	(VIDEO_CID_BASE + 26)
 /**
  * @}
  */
@@ -86,7 +86,7 @@ enum video_power_line_frequency {
  * @name Camera class controls IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CID_CAMERA_CLASS_BASE + 13)
+#define VIDEO_CID_ZOOM_ABSOLUTE			(VIDEO_CID_CAMERA_CLASS_BASE + 13)
 /**
  * @}
  */
@@ -95,7 +95,7 @@ enum video_power_line_frequency {
  * @name JPEG class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CID_JPEG_CLASS_BASE + 3)
+#define VIDEO_CID_JPEG_COMPRESSION_QUALITY	(VIDEO_CID_JPEG_CLASS_BASE + 3)
 /**
  * @}
  */
@@ -105,8 +105,8 @@ enum video_power_line_frequency {
  * @{
  */
 /** Pixel rate (pixels/second) in the device's pixel array. This control is read-only. */
-#define VIDEO_CID_PIXEL_RATE		(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 2)
-#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 3)
+#define VIDEO_CID_PIXEL_RATE			(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 2)
+#define VIDEO_CID_TEST_PATTERN			(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 3)
 /**
  * @}
  */