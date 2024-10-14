Johnsonyu0926/zephyr

drivers: video: controls: use the same classes IDs as Linux
The V4L2 and V4Z list of control IDs are slightly different. Use the
same CIDs as Linux, with the exact same numbers to facilitate debug
and co-development.

This also rename the GENERIC class into BASE as in Linux. No change
in the video CIDs names in this commit.

Signed-off-by: Josuah Demangeon <me@josuah.net>

include/zephyr/drivers/video-controls.h
@@ -1,11 +1,6 @@
-/**
- * @file
- *
- * @brief Public APIs for Video.
- */
-
 /*
  * Copyright (c) 2019 Linaro Limited.
+ * Copyright (c) 2024 tinyVision.ai Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,16 +8,28 @@
 #define ZEPHYR_INCLUDE_VIDEO_CONTROLS_H_
 
 /**
+ * @file
+ *
+ * @brief Public APIs for Video.
+ */
+
+/**
  * @brief Video controls
  * @defgroup video_controls Video Controls
  * @ingroup io_interfaces
+ *
+ * The Video control IDs (CIDs) are introduced with the same name as
+ * Linux V4L2 subsystem and under the same class. This facilitates
+ * inter-operability and debugging devices end-to-end across Linux and
+ * Zephyr.
+ *
  * @{
  */
 
-#include <zephyr/device.h>
 #include <stddef.h>
-#include <zephyr/kernel.h>
 
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
 #include <zephyr/types.h>
 
 #ifdef __cplusplus
@@ -31,13 +38,21 @@ extern "C" {
 
 /**
  * @name Control classes
+ *
+ * List of control classes, to group related controls together in blocks.
+ *
+ * This list is kept identical to the Linux kernel definitions.
+ *
  * @{
  */
-#define VIDEO_CTRL_CLASS_GENERIC	0x00000000	/**< Generic class controls */
-#define VIDEO_CTRL_CLASS_CAMERA		0x00010000	/**< Camera class controls */
-#define VIDEO_CTRL_CLASS_MPEG		0x00020000	/**< MPEG-compression controls */
-#define VIDEO_CTRL_CLASS_JPEG		0x00030000	/**< JPEG-compression controls */
-#define VIDEO_CTRL_CLASS_VENDOR		0xFFFF0000	/**< Vendor-specific class controls */
+#define VIDEO_CTRL_CLASS_BASE			0x00980900 /**< Base class controls */
+#define VIDEO_CTRL_CLASS_CODEC_BASE		0x00990900 /**< Stateful codec controls */
+#define VIDEO_CTRL_CLASS_CAMERA_BASE		0x009a0900 /**< Camera class controls */
+#define VIDEO_CTRL_CLASS_FLASH_BASE		0x009c0900 /**< Camera flash controls */
+#define VIDEO_CTRL_CLASS_JPEG_BASE		0x009d0900 /**< JPEG-compression controls */
+#define VIDEO_CTRL_CLASS_IMAGE_SOURCE_BASE	0x009e0900 /**< Image source controls */
+#define VIDEO_CTRL_CLASS_IMAGE_PROC_BASE	0x009f0900 /**< Image processing controls */
+#define VIDEO_CTRL_CLASS_VENDOR_BASE		0x00ff0900 /**< Vendor-specific class controls */
 /**
  * @}
  */
@@ -47,9 +62,9 @@ extern "C" {
  * @{
  */
 /** Mirror the picture horizontally */
-#define VIDEO_CID_HFLIP			(VIDEO_CTRL_CLASS_GENERIC + 0)
+#define VIDEO_CID_HFLIP			(VIDEO_CTRL_CLASS_BASE + 0)
 /** Mirror the picture vertically */
-#define VIDEO_CID_VFLIP			(VIDEO_CTRL_CLASS_GENERIC + 1)
+#define VIDEO_CID_VFLIP			(VIDEO_CTRL_CLASS_BASE + 1)
 /**
  * @}
  */
@@ -58,15 +73,15 @@ extern "C" {
  * @name Camera class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CTRL_CLASS_CAMERA + 0)
-#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CTRL_CLASS_CAMERA + 1)
-#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CTRL_CLASS_CAMERA + 2)
-#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CTRL_CLASS_CAMERA + 3)
-#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CTRL_CLASS_CAMERA + 4)
-#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CTRL_CLASS_CAMERA + 5)
-#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CTRL_CLASS_CAMERA + 6)
-#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CTRL_CLASS_CAMERA + 7)
-#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CTRL_CLASS_CAMERA + 8)
+#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CTRL_CLASS_CAMERA_BASE + 0)
+#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CTRL_CLASS_CAMERA_BASE + 1)
+#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CTRL_CLASS_CAMERA_BASE + 2)
+#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CTRL_CLASS_CAMERA_BASE + 3)
+#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CTRL_CLASS_CAMERA_BASE + 4)
+#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CTRL_CLASS_CAMERA_BASE + 5)
+#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CTRL_CLASS_CAMERA_BASE + 6)
+#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CTRL_CLASS_CAMERA_BASE + 7)
+#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CTRL_CLASS_CAMERA_BASE + 8)
 /**
  * @}
  */
@@ -74,9 +89,6 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-
-/* Controls */
-
 
 /**
  * @}

silverlee35/z_example

include/zephyr/drivers/video-controls.h
@@ -1,11 +1,6 @@
-/**
- * @file
- *
- * @brief Public APIs for Video.
- */
-
 /*
  * Copyright (c) 2019 Linaro Limited.
+ * Copyright (c) 2024 tinyVision.ai Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -13,16 +8,28 @@
 #define ZEPHYR_INCLUDE_VIDEO_CONTROLS_H_
 
 /**
+ * @file
+ *
+ * @brief Public APIs for Video.
+ */
+
+/**
  * @brief Video controls
  * @defgroup video_controls Video Controls
  * @ingroup io_interfaces
+ *
+ * The Video control IDs (CIDs) are introduced with the same name as
+ * Linux V4L2 subsystem and under the same class. This facilitates
+ * inter-operability and debugging devices end-to-end across Linux and
+ * Zephyr.
+ *
  * @{
  */
 
-#include <zephyr/device.h>
 #include <stddef.h>
-#include <zephyr/kernel.h>
 
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
 #include <zephyr/types.h>
 
 #ifdef __cplusplus
@@ -31,13 +38,21 @@ extern "C" {
 
 /**
  * @name Control classes
+ *
+ * List of control classes, to group related controls together in blocks.
+ *
+ * This list is kept identical to the Linux kernel definitions.
+ *
  * @{
  */
-#define VIDEO_CTRL_CLASS_GENERIC 0x00000000 /**< Generic class controls */
-#define VIDEO_CTRL_CLASS_CAMERA  0x00010000 /**< Camera class controls */
-#define VIDEO_CTRL_CLASS_MPEG    0x00020000 /**< MPEG-compression controls */
-#define VIDEO_CTRL_CLASS_JPEG    0x00030000 /**< JPEG-compression controls */
-#define VIDEO_CTRL_CLASS_VENDOR  0xFFFF0000 /**< Vendor-specific class controls */
+#define VIDEO_CID_BASE				0x00980900 /**< Base class controls */
+#define VIDEO_CID_CODEC_CLASS_BASE		0x00990900 /**< Stateful codec controls */
+#define VIDEO_CID_CAMERA_CLASS_BASE		0x009a0900 /**< Camera class controls */
+#define VIDEO_CID_FLASH_CLASS_BASE		0x009c0900 /**< Camera flash controls */
+#define VIDEO_CID_JPEG_CLASS_BASE		0x009d0900 /**< JPEG-compression controls */
+#define VIDEO_CID_IMAGE_SOURCE_CLASS_BASE	0x009e0900 /**< Image source controls */
+#define VIDEO_CID_IMAGE_PROC_CLASS_BASE		0x009f0900 /**< Image processing controls */
+#define VIDEO_CID_VENDOR_CLASS_BASE		0x00ff0900 /**< Vendor-specific class controls */
 /**
  * @}
  */
@@ -47,21 +62,19 @@ extern "C" {
  * @{
  */
 /** Mirror the picture horizontally */
-#define VIDEO_CID_HFLIP                (VIDEO_CTRL_CLASS_GENERIC + 0)
+#define VIDEO_CID_HFLIP			(VIDEO_CID_BASE + 0)
 /** Mirror the picture vertically */
-#define VIDEO_CID_VFLIP                (VIDEO_CTRL_CLASS_GENERIC + 1)
+#define VIDEO_CID_VFLIP			(VIDEO_CID_BASE + 1)
 /** Power line frequency (enum) filter to avoid flicker */
 #define VIDEO_CID_POWER_LINE_FREQUENCY (VIDEO_CTRL_CLASS_GENERIC + 2)
 /** Pixel rate (pixels/second) in the device's pixel array. This control is read-only. */
 #define VIDEO_CID_PIXEL_RATE           (VIDEO_CTRL_CLASS_GENERIC + 3)
-
 enum video_power_line_frequency {
 	VIDEO_CID_POWER_LINE_FREQUENCY_DISABLED = 0,
 	VIDEO_CID_POWER_LINE_FREQUENCY_50HZ = 1,
 	VIDEO_CID_POWER_LINE_FREQUENCY_60HZ = 2,
 	VIDEO_CID_POWER_LINE_FREQUENCY_AUTO = 3,
 };
-
 /**
  * @}
  */
@@ -70,16 +83,16 @@ enum video_power_line_frequency {
  * @name Camera class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_EXPOSURE     (VIDEO_CTRL_CLASS_CAMERA + 0)
-#define VIDEO_CID_CAMERA_GAIN         (VIDEO_CTRL_CLASS_CAMERA + 1)
-#define VIDEO_CID_CAMERA_ZOOM         (VIDEO_CTRL_CLASS_CAMERA + 2)
-#define VIDEO_CID_CAMERA_BRIGHTNESS   (VIDEO_CTRL_CLASS_CAMERA + 3)
-#define VIDEO_CID_CAMERA_SATURATION   (VIDEO_CTRL_CLASS_CAMERA + 4)
-#define VIDEO_CID_CAMERA_WHITE_BAL    (VIDEO_CTRL_CLASS_CAMERA + 5)
-#define VIDEO_CID_CAMERA_CONTRAST     (VIDEO_CTRL_CLASS_CAMERA + 6)
-#define VIDEO_CID_CAMERA_TEST_PATTERN (VIDEO_CTRL_CLASS_CAMERA + 7)
-#define VIDEO_CID_CAMERA_QUALITY      (VIDEO_CTRL_CLASS_CAMERA + 8)
-#define VIDEO_CID_CAMERA_HUE          (VIDEO_CTRL_CLASS_CAMERA + 9)
+#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CID_CAMERA_CLASS_BASE + 0)
+#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CID_CAMERA_CLASS_BASE + 1)
+#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CID_CAMERA_CLASS_BASE + 2)
+#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CID_CAMERA_CLASS_BASE + 3)
+#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CID_CAMERA_CLASS_BASE + 4)
+#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CID_CAMERA_CLASS_BASE + 5)
+#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CID_CAMERA_CLASS_BASE + 6)
+#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CID_CAMERA_CLASS_BASE + 7)
+#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CID_CAMERA_CLASS_BASE + 8)
+#define VIDEO_CID_CAMERA_HUE		(VIDEO_CID_CAMERA_CLASS_BASE + 9)
 /**
  * @}
  */
@@ -87,8 +100,6 @@ enum video_power_line_frequency {
 #ifdef __cplusplus
 }
 #endif
-
-/* Controls */
 
 /**
  * @}