719ef18fb467f4bfb4050a90d72e4bc820d0ef77
Declan Snyder <declan.snyder@nxp.com>
2024-10-16 15:44:47 -0500

    drivers: dma_mcux_edma: Fix bool check macro
    Change a HAS_PROP macro to just PROP, this is common error for checking
    boolean value from DT.
    
    Signed-off-by: Declan Snyder <declan.snyder@nxp.com>

--- drivers/dma/dma_mcux_edma.c
+++ drivers/dma/dma_mcux_edma.c
@@ -675,7 +675,7 @@ static int dma_mcux_edma_init(const struct device *dev)
 		LISTIFY(NUM_IRQS_WITHOUT_ERROR_IRQ(n),				\
 			DMA_MCUX_EDMA_IRQ_CONFIG, (;), n)			\
 										\
-		IF_ENABLED(UTIL_NOT(DT_INST_NODE_HAS_PROP(n, no_error_irq)),	\
+		IF_ENABLED(UTIL_NOT(DT_INST_PROP(n, no_error_irq)),		\
 			   (IRQ_CONFIG(n, NUM_IRQS_WITHOUT_ERROR_IRQ(n),	\
 			    dma_mcux_edma_error_irq_handler)))			\
 										\
